0328fbd3d50cd4040f9d5e3331c2b3e2e26d51ef
Oscar Luntha <lunthaoscar@gmail.com>
2025-11-25 17:03:17 +0200

    create a function for getting grid size from user

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let container = document.querySelector(".container")
-for(let i = 1; i<17; i++ ){
-    for (let j=1; j<17; j++){
-        let box = document.createElement("div")
-        box.setAttribute("class","box")
-        box.addEventListener("mouseover",() => {box.classList.add("paint")})
-        container.appendChild(box)
-    }
+let btn = document.querySelector("button")
+let defaultGrid = 16;
+btn.addEventListener("click", () => {
+    getGridSize()
+})
+function getGridSize(){
+    gridSize = prompt("Enter grid size in range 1 - 100");
+    console.log(gridSize)
+    return gridSize;
+
 }
