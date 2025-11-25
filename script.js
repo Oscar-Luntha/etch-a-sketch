73980da51a780906c49e6404b7bd8f8b84074e73
Oscar Luntha <lunthaoscar@gmail.com>
2025-11-25 15:56:17 +0200

    add a hover event listener that appends a class name to the box

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ for(let i = 1; i<17; i++ ){
     for (let j=1; j<17; j++){
         let box = document.createElement("div")
         box.setAttribute("class","box")
+        box.addEventListener("mouseover",() => {box.classList.add("paint")})
         container.appendChild(box)
     }
 }
