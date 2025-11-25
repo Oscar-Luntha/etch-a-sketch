7bb92fa31e1ff2ef2e62f145dc59d1c4222f2897
Oscar Luntha <lunthaoscar@gmail.com>
2025-11-25 15:21:25 +0200

    create a grid of 16 X 16 boxes and append them to the container node

---
+++ script.js
@@ -0,0 +1,8 @@
+let container = document.querySelector(".container")
+for(let i = 1; i<17; i++ ){
+    for (let j=1; j<17; j++){
+        let box = document.createElement("div")
+        box.setAttribute("class","box")
+        container.appendChild(box)
+    }
+}
