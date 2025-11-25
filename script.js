dbc53f0b5b136e75fd67ce59036e612043f9280f
Oscar Luntha <lunthaoscar@gmail.com>
2025-11-25 19:45:24 +0200

    draw the grid into the container

--- script.js
+++ script.js
@@ -15,9 +15,22 @@ function getGridSize(){
     return gridSize;
 }
 function drawGrid(gridSize){
+    container.innerHTML = "";
     let position = container.getBoundingClientRect()
     let containerWidth = position.width
     let containerHeight = position.height
-    let boxWidth = containerWidth / gridSize
-    let boxHeight = containerHeight / gridSize    
+    let boxWidth =containerWidth / gridSize
+    let boxHeight = containerHeight/ gridSize
+    for (let i = 0; i < gridSize; i++) {
+        console.log(containerWidth)
+        console.log(boxWidth)
+    for (let j = 0; j < gridSize; j++) {  
+        let box = document.createElement("div");
+        box.classList.add("gridBox");
+        box.style.width = boxWidth + "px";
+        box.style.height = boxHeight+ "px";
+        container.appendChild(box);
+        }
+    }
+
 }
